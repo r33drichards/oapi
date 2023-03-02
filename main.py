2844cbc4e9aa25eabcd95999dfdcfa4ac7d98623
r33drichards <rwendt1337@gmail.com>
2023-03-02 22:48:35 +0000

    working example

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from pprint import pprint
 
 configuration = swagger_client.Configuration()
 
-configuration.api_key = "ol_api_rR5xQ5s3Ew1RyDJR5shCqw79dJPo8kgD6X8vbI"
+token = 'ol_api_rR5xQ5s3Ew1RyDJR5shCqw79dJPo8kgD6X8vbI'
+
 
 
 # create an instance of the API class
-api_instance = swagger_client.DocumentsApi(swagger_client.ApiClient(configuration))
+api_instance = swagger_client.DocumentsApi(swagger_client.ApiClient(
+    configuration,
+    header_name='Authorization',
+    header_value=f'Bearer {token}'
+))
 
 
 api_response = api_instance.documents_list_post()
